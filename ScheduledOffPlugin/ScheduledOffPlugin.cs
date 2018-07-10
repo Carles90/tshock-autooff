ea718ef40d905ec8994f9a2dd57d73afddad3e4f
Carles Mañas <carles.manas.nicolas@gmail.com>
2018-07-10 22:15:30 +0200

    First commit

---
+++ ScheduledOffPlugin/ScheduledOffPlugin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace ScheduledOffPlugin
+{
+    /// <summary>
+    /// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 1.25
+    /// </summary>
+    [ApiVersion(2, 1)]
+    public class ScheduledOffPlugin : TerrariaPlugin
+    {
+        /// <summary>
+        /// The name of the plugin.
+        /// </summary>
+        public override string Name => "ScheduledOff Plugin";
+
+        /// <summary>
+        /// The version of the plugin in its current state.
+        /// </summary>
+        public override Version Version => new Version(1, 0, 0);
+
+        /// <summary>
+        /// The author(s) of the plugin.
+        /// </summary>
+        public override string Author => "Carles Mañas";
+
+        /// <summary>
+        /// A short, one-line, description of the plugin's purpose.
+        /// </summary>
+        public override string Description => "";
+
+        /// <summary>
+        /// The plugin's constructor
+        /// Set your plugin's order (optional) and any other constructor logic here
+        /// </summary>
+        public ScheduledOffPlugin(Main game) : base(game)
+        {
+
+        }
+
+        /// <summary>
+        /// Performs plugin initialization logic.
+        /// Add your hooks, config file read/writes, etc here
+        /// </summary>
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command("scoff", ScheduledOff, "scoff") { HelpText = "Schedule an automatic server shutdown" });
+        }
+        
+        /// <summary>
+        /// Performs plugin cleanup logic
+        /// Remove your hooks and perform general cleanup here
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+            base.Dispose(disposing);
+        }
+
+        private static void ScheduledOff(CommandArgs args)
+        {
+
+        }
+    }
+}
